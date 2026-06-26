ae9983ef6e407ed033d0aa379d2fafa625003426
OSCAR <OSCAR@MSI>
2026-06-26 03:17:11 -0600

    INTERFAZ

--- src/CONTROLADOR/ControladorAutomovil.java
+++ src/CONTROLADOR/ControladorAutomovil.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package CONTROLADOR;
-
+//K
 import Datos.RepositorioDatos;
 import MODELO.Automovil;
 import MODELO.Cliente;
